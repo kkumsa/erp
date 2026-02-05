a0734943cb14405f8d44a5421c3e8350a35fd919
kkumsa <kkumsa@luvd.co.kr>
2026-02-05 21:46:13 +0900

    fix: fix api.php

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
-    // 고객 API
-    Route::apiResource('customers', \App\Http\Controllers\Api\CustomerController::class);
-
-    // 프로젝트 API
-    Route::apiResource('projects', \App\Http\Controllers\Api\ProjectController::class);
-
-    // 청구서 API
-    Route::apiResource('invoices', \App\Http\Controllers\Api\InvoiceController::class);
 });
